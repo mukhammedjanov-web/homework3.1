16ae8726d34950034b93a4f18d8367c9fb143f35
mukhammedjanov-web <akmalmuhammadjonov6@gmail.com>
2022-09-14 21:26:54 +0500

    homework 3-1

---
+++ script.js
@@ -0,0 +1,58 @@
+
+// 1-m
+// let L=prompt("L uzunlikni kiriting (santimetrda)");
+
+// console.log(`To'liq metrlar ${Math.trunc(L/100)} metr`);
+
+
+// 2-m
+// let M=prompt("M og'irlikni kiriting (kilogrammda)");
+
+// console.log(`To'liq tonnalar ${Math.trunc(M/1000)} tonna`);
+
+
+// 3-m
+// let A=prompt("A sonni kiriting");
+// let B=prompt("B sonni kiriting (B<A)")
+
+// console.log(`A kesmada B kesmani ${Math.trunc(A/B)} martta joylash mumkin`);
+
+
+// 4-m
+// let A=prompt("Ikki xonali son kiriting");
+
+
+// console.log(`O'nliklar xonasidagi raqam ${A[0]}`);
+// console.log(`Birliklar xonasidagi raqam ${A[1]}`);
+
+
+// 5-m
+// let A=prompt("Ikki xonali son kiriting");
+
+
+// console.log(`Ushbu sonning raqamlar yig'indisi ${A[0]*1+A[1]*1}`);
+
+
+// 6-m
+// let A=prompt("Ikki xonali son kiriting");
+
+
+// console.log(`Ushbu sonning raqamlar ko'paytmasi ${A[0]*1*A[1]*1}`);
+
+
+// 7-m
+// let A=prompt("999 dan katta son kiriting");
+
+
+// console.log(`Ushbu sonda ${Math.trunc(A/100)}ta yuzlar mavjud `);
+
+
+
+// 8-m
+// let N=prompt("Kun boshidan nechchi sekund o'tdi");
+
+
+// console.log(`Kun boshidan buyon ${Math.trunc(N/3600)} soatu ${Math.trunc((N%3600)/60)} daqiqa o'tdi`);
+
+
+
